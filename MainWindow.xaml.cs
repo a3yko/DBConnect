4ad47a8a7225ea132abf4cae4348fdedb57fc80a
Adrian Atanasov <adriansoccar@gmail.com>
2021-02-25 23:23:16 -0600

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Data;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Database_Connector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Execute_Click(object sender, RoutedEventArgs e)
+        {
+            if (combo.SelectedIndex == 0)
+            {
+                Server sql = new Server(user.Text, passsword.Password.ToString(), ip.Text, db.Text);
+                DataTable table = sql.RunSQLQuery(input.Text);
+                if (table.Rows.Count == 0)
+                {
+                    MessageBox.Show("No Data Received");
+                }
+                output.ItemsSource = table.DefaultView;
+            }
+            else if (combo.SelectedIndex == 1)
+            {
+                Server sql = new Server(user.Text, passsword.Password.ToString(), ip.Text, db.Text);
+                DataTable table = sql.Runpsql(input.Text);
+                if (table.Rows.Count == 0)
+                {
+                    MessageBox.Show("No Data Received");
+                }
+                output.ItemsSource = table.DefaultView;
+            }
+        }
+
+        private void OpenQuery(object sender, RoutedEventArgs e)
+        {
+           OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                input.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Npgsql;
+
+namespace Database_Connector
+{
+    class Server
+    {
+        private DataTable output = new DataTable();
+        private string username;
+        private string password;
+        private string db;
+        private string ip;
+        public Server(string username, string password, string ip, string db)
+        {
+            this.username = username;
+            this.password = password;
+            this.db = db;
+            this.ip = ip;
+        }
+
+        public DataTable RunSQLQuery(string query)
+        {
+            string connStr = "server=" + this.ip + ";user=" + this.username + ";database=" + this.db + ";port=3306;password=" + this.password;
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                using(MySqlDataAdapter a = new MySqlDataAdapter(cmd))
+                {
+                    a.Fill(output);
+                }
+                conn.Close();
+                Console.WriteLine("Done.");
+                return output;
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return output;
+            }
+       
+
+        }
+
+        public DataTable Runpsql(string query)
+        {
+            string connStr = "server=" + this.ip + ";user=" + this.username + ";database=" + this.db + ";port=3306;password=" + this.password;
+            var conn = new NpgsqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to PostgresSQL....");
+                conn.Open();
+
+                NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                using(NpgsqlDataAdapter a = new NpgsqlDataAdapter(cmd))
+                {
+                    a.Fill(output);
+                }
+                conn.Close();
+                Console.WriteLine("Done.");
+                return output;
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return output;
+            }
+            
+        }
+
+    }
+}
